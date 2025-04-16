92d0c63a56eb4988e462f6fa9f7106e2b2f3b6a5
Keyvan Mahmoudi <keyvanmahmoudi72@gmail.com>
2025-04-16 13:52:42 +0200

    Do the exercise

--- src/modules/task-options/task-options.module.ts
+++ src/modules/task-options/task-options.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TaskOptionsService } from './task-options.service';
 import { TaskOptionsController } from './task-options.controller';
-// import { TASK_OPTIONS_TOKEN, taskOptions } from './constants';
+import { TASK_OPTIONS_TOKEN } from './constants';
+import { TaskOptionsProvider } from './task-options.provider';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [TaskOptionsController],
   providers: [
     { provide: TaskOptionsService, useClass: TaskOptionsService },
-    // {
-    //   provide:
-    //   useValue:
-    // },
+    {
+      provide: TASK_OPTIONS_TOKEN,
+      useFactory: TaskOptionsProvider,
+      inject: [ConfigService],
+    },
   ],
 })
 export class TaskOptionsModule {}
--- src/modules/task-options/task-options.provider.ts
+++ src/modules/task-options/task-options.provider.ts
@@ -1,6 +1,11 @@
-import { taskOptions } from './constants';
+import { ConfigService } from '@nestjs/config';
 
-export const TaskOptionsProvider = (): string[] => {
+export const TaskOptionsProvider = (configService: ConfigService): string[] => {
   // FIXME: use the configService to get the task options from the environment variable instead. It is a string so map it to a proper array of strings
+  const taskOptionsEnv = configService.get<string>('TASK_OPTIONS');
+  if (!taskOptionsEnv) {
+    return [];
+  }
+  const taskOptions = taskOptionsEnv.split(',').map((option) => option.trim());
   return taskOptions;
 };
